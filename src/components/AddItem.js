cdaab844d5ffa634e664a03173443b853b1d869b
Cameron Dunning <dunningcameron@gmail.com>
2022-02-15 20:15:38 -0500

    Fix form issues

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -14,29 +14,34 @@ import {
 const AddItem = () => {
   const [newGrocery, setNewGrocery] = useState("");
 
-  const createGrocery = async () => {
-    const groceries = ref(db, "/groceries");
-    push(groceries, {
-      grocery: newGrocery,
-      regular: false,
-      check1: false,
-      check2: false,
-    });
+  const createGrocery = async (event) => {
+    event.preventDefault();
+    if (newGrocery) {
+      const groceries = ref(db, "/groceries");
+      push(groceries, {
+        grocery: newGrocery,
+        regular: false,
+        check1: false,
+        check2: false,
+      });
+    }
+    setNewGrocery("");
   };
 
   return (
     <Container>
-      <Form>
+      <Form onSubmit={(event) => createGrocery(event)}>
         <InputGroup className="mb-3">
           <FormControl
             placeholder="Add Item"
             aria-label="Add Item"
             aria-describedby="basic-addon2"
+            value={newGrocery}
             onChange={(event) => {
               setNewGrocery(event.target.value);
             }}
           />
-          <Button variant="primary" onClick={createGrocery}>
+          <Button variant="primary" type="submit">
             Add Item
           </Button>
         </InputGroup>
